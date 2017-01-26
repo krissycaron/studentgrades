1502ffe432c4d2138a4cc7395004b1037efca3d4
Kristin Caron <krissycaron@gmail.com>
2017-01-25 19:21:08 -0600

    first branch commit

---
+++ grades.js
@@ -0,0 +1,22 @@
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+
+var gradeA = 0
+var gradeB = 0
+var gradeC = 0
+var gradeD = 0
+var gradeF = 0 
+
+
+
+for (var i=0; i<scores.length; i++) {
+
+
+
+}
+
+
+
+
+
+
+
